30bcbf814694be7d106918412b35386286dad6b3
Harnam Sandhu <harnamsinghsandhu04@gmail.com>
2017-09-23 23:06:38 +0530

    Auto-Reload
    Added auto-reload when both player die..

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject p1;
+	public GameObject p2;
+	public Button restart;
+	public Button quit;
+	bool chuk = false;
+
+	// Use this for initialization
+	void Start () {
+		restart.gameObject.SetActive (false);
+		quit.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!chuk) {
+			if (!p1.activeSelf) {
+				if (!p2.activeSelf) {
+					//restart.gameObject.SetActive (true);
+					//quit.gameObject.SetActive (true);
+					//chuk = true;
+					Scene scene = SceneManager.GetActiveScene ();
+					SceneManager.LoadScene (scene.name);
+				}
+			}
+		}
+	}
+
+	public void res(){
+		Scene scene = SceneManager.GetActiveScene ();
+		Debug.Log (scene);
+		Debug.Log ("1");
+		SceneManager.LoadScene (scene.name);
+	}
+
+	public void qui(){
+		Application.Quit ();
+	}
+}
