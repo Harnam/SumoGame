8b3e4f58edd362ce5644fc888ae5d865b9a23782
Harnam Sandhu <harnamsinghsandhu04@gmail.com>
2017-09-24 15:53:13 +0530

    Pause
    Added a pause menu..
    Added quit button in pause menu..
    Added Reset Score Button in pause menu..

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,11 +10,20 @@ public class GameManager : MonoBehaviour {
 	public Button restart;
 	public Button quit;
 	bool chuk = false;
+	public Button quitgame;
+	public Button unpause;
+	public Button reset;
+	public Image bg;
+	bool ispaused = false;
 
 	// Use this for initialization
 	void Start () {
 		restart.gameObject.SetActive (false);
 		quit.gameObject.SetActive (false);
+		quitgame.gameObject.SetActive (false);
+		unpause.gameObject.SetActive (false);
+		reset.gameObject.SetActive (false);
+		bg.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -30,8 +39,27 @@ public class GameManager : MonoBehaviour {
 				}
 			}
 		}
-		if (Input.GetKey (KeyCode.Escape)) {
-			Application.Quit ();
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Debug.Log ("Key Up");
+			if (!ispaused) {
+				quitgame.gameObject.SetActive (true);
+				unpause.gameObject.SetActive (true);
+				reset.gameObject.SetActive (true);
+				bg.gameObject.SetActive (true);
+				p1.gameObject.GetComponent<PlayerMovement> ().enabled = false;
+				p2.gameObject.GetComponent<PlayerMovement> ().enabled = false;
+				ispaused = true;
+			} else if (ispaused) {
+				quitgame.gameObject.SetActive (false);
+				unpause.gameObject.SetActive (false);
+				reset.gameObject.SetActive (false);
+				bg.gameObject.SetActive (false);
+				p1.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+				p2.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+				ispaused = false;
+			} else {
+				Debug.Log("Noob");
+			}
 		}
 	}
 
@@ -43,6 +71,32 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void qui(){
+		Application.Quit ();
+	}
+
+	public void unpau(){
+		quitgame.gameObject.SetActive (false);
+		unpause.gameObject.SetActive (false);
+		reset.gameObject.SetActive (false);
+		bg.gameObject.SetActive (false);
+		p1.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+		p2.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+		ispaused = false;
+	}
+
+	public void resetscore(){
+		PlayerPrefs.SetInt ("BlueScore", 0);
+		PlayerPrefs.SetInt ("RedScore", 0);
+		quitgame.gameObject.SetActive (false);
+		unpause.gameObject.SetActive (false);
+		reset.gameObject.SetActive (false);
+		bg.gameObject.SetActive (false);
+		p1.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+		p2.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+		ispaused = false;
+	}
+
+	public void quigame(){
 		Application.Quit ();
 	}
 }
