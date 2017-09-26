f66f22aaebeedcdac0c339d148e7c6f9c6457658
Harnam Sandhu <harnamsinghsandhu04@gmail.com>
2017-09-26 12:39:45 +0530

    Quality Movement Speed
    Fixed Movement speed of players increasing with high fps and decreasing with low fps..

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,40 +6,43 @@ public class PlayerMovement : MonoBehaviour {
 	public int valueforce;
 	public Rigidbody rb;
 	public int defaultcon;
+	public float speedval;
+	float speed;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		speed = speedval * Time.deltaTime * 0.01f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (defaultcon == 0) {
 			if (Input.GetKey("up")){
-				transform.position = new Vector3 (transform.position.x + 0.05f, transform.position.y, transform.position.z);
+				transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
 			}
 			if (Input.GetKey("down")){
-				transform.position = new Vector3 (transform.position.x - 0.05f, transform.position.y, transform.position.z);
+				transform.position = new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z);
 			}
 			if (Input.GetKey("left")){
-				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 0.05f);
+				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + speed);
 			}
 			if (Input.GetKey("right")){
-				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 0.05f );
+				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - speed );
 			}
 		}
 		if (defaultcon == 1) {
 			if (Input.GetKey(KeyCode.W)){
-				transform.position = new Vector3 (transform.position.x + 0.05f, transform.position.y, transform.position.z);
+				transform.position = new Vector3 (transform.position.x + speed, transform.position.y, transform.position.z);
 			}
 			if (Input.GetKey(KeyCode.S)){
-				transform.position = new Vector3 (transform.position.x - 0.05f, transform.position.y, transform.position.z );
+				transform.position = new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z );
 			}
 			if (Input.GetKey(KeyCode.A)){
-				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + 0.05f);
+				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + speed);
 			}
 			if (Input.GetKey(KeyCode.D)){
-				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 0.05f);
+				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - speed);
 			}
 		}
 	}
