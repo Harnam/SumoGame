0cb2ff480940c70cd584f24363e4a694905a38d7
Harnam Sandhu <harnamsinghsandhu04@gmail.com>
2017-09-28 01:04:42 +0530

    3, 2, 1, Fight
    Added 3, 2, 1, Fight..
    Added pause will not work if fight not started..

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,11 @@ public class GameManager : MonoBehaviour {
 	public Image bg;
 	public Button gotomain;
 	bool ispaused = false;
+	public RawImage fightimg;
+	public Text txt1;
+	public Text txt2;
+	public Text txt3;
+	bool isload = false;
 
 	// Use this for initialization
 	void Start () {
@@ -27,6 +32,19 @@ public class GameManager : MonoBehaviour {
 		gotomain.gameObject.SetActive (false);
 		bg.gameObject.SetActive (false);
 		Cursor.visible = false;
+		p1.gameObject.GetComponent<PlayerMovement> ().enabled = false;
+		p2.gameObject.GetComponent<PlayerMovement> ().enabled = false;
+		txt2.gameObject.SetActive (false);
+		txt1.gameObject.SetActive (false);
+		fightimg.gameObject.SetActive (false);
+		Destroy (txt3, 1);
+		Destroy (txt2, 2);
+		Destroy (txt1, 3);
+		Destroy (fightimg, 4);
+		StartCoroutine(playerenable());
+		StartCoroutine(txt1enable());
+		StartCoroutine(txt2enable());
+		StartCoroutine(imgenable());
 	}
 	
 	// Update is called once per frame
@@ -42,30 +60,32 @@ public class GameManager : MonoBehaviour {
 				}
 			}
 		}
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			Debug.Log ("Key Up");
-			if (!ispaused) {
-				quitgame.gameObject.SetActive (true);
-				unpause.gameObject.SetActive (true);
-				reset.gameObject.SetActive (true);
-				gotomain.gameObject.SetActive (true);
-				bg.gameObject.SetActive (true);
-				p1.gameObject.GetComponent<PlayerMovement> ().enabled = false;
-				p2.gameObject.GetComponent<PlayerMovement> ().enabled = false;
-				ispaused = true;
-				Cursor.visible = true;
-			} else if (ispaused) {
-				quitgame.gameObject.SetActive (false);
-				unpause.gameObject.SetActive (false);
-				reset.gameObject.SetActive (false);
-				bg.gameObject.SetActive (false);
-				gotomain.gameObject.SetActive (false);
-				p1.gameObject.GetComponent<PlayerMovement> ().enabled = true;
-				p2.gameObject.GetComponent<PlayerMovement> ().enabled = true;
-				ispaused = false;
-				Cursor.visible = false;
-			} else {
-				Debug.Log("Noob");
+		if(isload){
+			if (Input.GetKeyDown (KeyCode.Escape)) {
+				Debug.Log ("Key Up");
+				if (!ispaused) {
+					quitgame.gameObject.SetActive (true);
+					unpause.gameObject.SetActive (true);
+					reset.gameObject.SetActive (true);
+					gotomain.gameObject.SetActive (true);
+					bg.gameObject.SetActive (true);
+					p1.gameObject.GetComponent<PlayerMovement> ().enabled = false;
+					p2.gameObject.GetComponent<PlayerMovement> ().enabled = false;
+					ispaused = true;
+					Cursor.visible = true;
+				} else if (ispaused) {
+					quitgame.gameObject.SetActive (false);
+					unpause.gameObject.SetActive (false);
+					reset.gameObject.SetActive (false);
+					bg.gameObject.SetActive (false);
+					gotomain.gameObject.SetActive (false);
+					p1.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+					p2.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+					ispaused = false;
+					Cursor.visible = false;
+				} else {
+					Debug.Log ("Noob");
+				}
 			}
 		}
 	}
@@ -113,6 +133,29 @@ public class GameManager : MonoBehaviour {
 
 	public void mainmen(){
 		SceneManager.LoadScene ("mainmenu");
+	}
+
+	IEnumerator playerenable(){
+		yield return new WaitForSeconds(4);
+		p1.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+		p2.gameObject.GetComponent<PlayerMovement> ().enabled = true;
+		Debug.Log ("START");
+		isload = true;
+	}
+
+	IEnumerator txt2enable(){
+		yield return new WaitForSeconds(1);
+		txt2.gameObject.SetActive (true);
+	}
+
+	IEnumerator txt1enable(){
+		yield return new WaitForSeconds(2);
+		txt1.gameObject.SetActive (true);
+	}
+
+	IEnumerator imgenable(){
+		yield return new WaitForSeconds(3);
+		fightimg.gameObject.SetActive (true);
 	}
 
 }
